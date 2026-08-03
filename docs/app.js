3c6d73521a8447561eea0edd67a9d2979e8b61ab
coopofficialaaron-cmd <coop.official.aaron@gmail.com>
2026-08-03 20:16:25 +0100

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -506,10 +506,7 @@ function renderMarket(){
       const div=document.createElement("div");
       div.className="pill breadth "+cfg[1];
       div.innerHTML=`<b>宽度</b> ${Math.round(bd.pct*100)}% <span class="arrow">${cfg[0]}</span>`;
-      div.title=`大盘宽度 ${Math.round(bd.pct*100)}%（${bd.ok}/${bd.n} 个标的的基准 ETF 向上）&#10;`+
-        `≥60% 牛 / 45~60% 中性 / <45% 熊&#10;`+
-        `中性档是回测里唯一亏钱的区间（PF 0.91）&#10;`+
-        `熊市档 ATR% 阶梯反转：2.5~4% 最好，强档失效`;
+      div.title=`${bd.ok}/${bd.n} 个标的的基准 ETF 向上`;
       slot.appendChild(div);
     }
   }
